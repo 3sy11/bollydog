c0315f0a6d87b58c77de769a6481ef6743e94fe9
3sy11 <socerer0001@hotmail.com>
2024-12-16 12:20:39 +0800

    ftr: add file protocol

--- bollydog/adapters/local.py
+++ bollydog/adapters/local.py
@@ -17,16 +17,36 @@ class LogProtocol(Protocol):
 
 class FileUnitOfWork(UnitOfWork):
 
-    def __init__(self, path):
-        self.path = path
+    def __init__(self, path: str | pathlib.Path):
+        if isinstance(path, str):
+            self.path = pathlib.Path(path)
+        else:
+            self.path = path
         super().__init__()
 
     def create(self):
-        return open(self.path, 'w+')
+        return True
+
+    @asynccontextmanager
+    async def connect(self, filename) -> AsyncGenerator:
+        file = self.path / filename
+        with open(file.as_posix(), 'a+') as f:
+            yield f
 
 
 class FileProtocol(Protocol):
-    ...
+
+    async def write(self, filename, text):
+        async with self.unit_of_work.connect(filename) as f:
+            f.write(text)
+        return True
+
+    async def read(self, filename):
+        file = self.unit_of_work.path / filename
+        if not file.exists():
+            return None
+        async with self.unit_of_work.connect(filename) as f:
+            return f.read()
 
 
 class NoneUnitOfWork(UnitOfWork):
